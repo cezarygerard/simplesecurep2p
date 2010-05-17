67c69ea2c9b38a457e6835b5dead97681d23f144
czarek.zawadka <czarek.zawadka@fa7ed45d-7e5d-be58-e22f-04897de4f73b>
2010-05-17 23:46:57 +0000

    Pierwsza klasa

---
+++ src/main/SimpleSecureP2P.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author czarek
+ *
+ */
+public class SimpleSecureP2P {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		  System.out.println("Hello, Project");
+
+	}
+
+}
