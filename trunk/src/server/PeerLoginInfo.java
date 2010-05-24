e3c9019b2430b3a2973a9a3961948bfd75d15506
czarek.zawadka <czarek.zawadka@fa7ed45d-7e5d-be58-e22f-04897de4f73b>
2010-05-24 11:19:27 +0000

    zmieniona struktura katalogow, pobieranie zahaszowanych hasel

--- trunk/src/server/PeerLoginInfo.java
+++ trunk/src/server/PeerLoginInfo.java
@@ -1,12 +1,19 @@
 package server;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * @author czarek
  *	
  */
 public class PeerLoginInfo implements Serializable, Comparable<PeerLoginInfo> {
+
+	@Override
+	public String toString() {
+		return "PeerLoginInfo [login=" + login + ", passwdHash="
+				+ Arrays.toString(passwdHash) + "]";
+	}
 
 	private static final long serialVersionUID = 1L;
 	String login;
