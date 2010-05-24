ff8a7dea0fabc9d5b58ffdfe59972a4d110ff14d
czarek.zawadka <czarek.zawadka@fa7ed45d-7e5d-be58-e22f-04897de4f73b>
2010-05-24 11:34:50 +0000

    szkic protokolu peer-server

--- trunk/src/common/PeerServerProtocol.java
+++ trunk/src/common/PeerServerProtocol.java
@@ -1,8 +1,40 @@
 package common;
 
 public class PeerServerProtocol {
+	
+	//Peer
+	static final String LOGIN = "login";
 
+	//Server
+	static final String LOGGEDIN = "loggedin";
 
-//	    static final String 
-
+	//Server
+	static final String LOGINFAILED = "loginfailed";
+	
+	//Server
+	static final String GETYOURINFO = "getyourinfo";
+	
+	//Peer
+	static final String MYINFO = "myinfo";
+	
+	//Peer
+	static final String GETCERT = "getcert";
+	
+	//Server
+	static final String CERT = "cert";
+		
+	//Peer
+	static final String GETPEERSINFO = "getpeers";
+		
+	//Server
+	static final String PEERSINFO = "peersinfo";
+	
+	//Peer or Server
+	static final String FAILURE = "failure";
+	
+	//Peer or Server
+	static final String TIMEOUT = "timeout";
+	
+	//Peer or Server
+	static final String EXIT = "exit";
 }
