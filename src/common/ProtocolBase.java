6f75d6db9d911209d4733f825f11c126e3ef1b75
czarek.zawadka <czarek.zawadka@fa7ed45d-7e5d-be58-e22f-04897de4f73b>
2010-05-29 20:45:20 +0000

    TEraz dopeiro powinno dzialac + kilka drobnych bugow + klasa ProtocolBase ktora ma wszpolne pola klas protokolow

---
+++ src/common/ProtocolBase.java
@@ -0,0 +1,18 @@
+package common;
+
+public class ProtocolBase {
+
+	//Peer or Server
+	public static final String FAILURE = "failure";
+	
+	//Peer or Server
+	public static final String OK = "ok";
+	
+	
+	//Peer or Server
+	public static final String TIMEOUT = "timeout";
+	
+	//Peer or Server
+	public static final String EXIT = "exit";
+
+}
